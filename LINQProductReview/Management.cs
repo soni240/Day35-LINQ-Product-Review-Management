dac7b4121d900f620a3d781a5572d96c854ce06c
soni240 <s.fegade21@gmail.com>
2022-07-27 12:03:48 +0530

    [Gunavanti]Add:retrieve top 3 records

--- LINQProductReview/Management.cs
+++ LINQProductReview/Management.cs
@@ -24,4 +24,39 @@ namespace LINQProductReview
 
         }
     }
+    public void SelectedRecords(List<ProductReview> listProductReview)
+    {
+        var recordedData = from productReviews in listProductReview
+                           where (productReviews.ProducID == 1 || productReviews.ProducID == 4 || productReviews.ProducID == 9)
+                           && productReviews.Rating > 3
+                           select productReviews;
+        //Console.WriteLine(recordedData);
+        foreach (var list in recordedData)
+        {
+            Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+        }
+
+    }
+
+    public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+    {
+        var recordedData = listProductReview.GroupBy(y => y.ProducID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+
+
+        foreach (var list in recordedData)
+        {
+            Console.WriteLine(list.ProductID + "--------" + list.Count);
+
+        }
+    }
+    public void RetriveProductIdAndReview(List<ProductReview> listProductReview)
+    {
+        var recordedData = (from productReviews in listProductReview select productReviews);
+        foreach (var list in recordedData)
+        {
+            Console.WriteLine("ProductID:- " + list.ProducID + "Review:- " + list.Review + " ");
+        }
+    }
+
 }
